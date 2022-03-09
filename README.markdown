4de9b530e4021f02354f68106c322ea656235c68
Ricardo <ricardosousa339@gmail.com>
2022-03-09 17:28:34 -0300

    second commit

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Calculadora de Média Imersão Dev Alura
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Rickk_316/pen/podmQVb](https://codepen.io/Rickk_316/pen/podmQVb).
+
+
---
+++ dist/script.js
@@ -0,0 +1,16 @@
+var nome = "Ricardo"
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 5
+var notaDoTerceiroBimestre = 9
+var notaDoQuartoBimestre = 4
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre)/4
+
+var notaFixada = notaFinal.toFixed(2)
+
+console.log("Bem vindo "+ nome)
+console.log(notaFixada)
+
+// Revisão
+
+//Variáveis, strings, console.log, toFixed, operações matemáticas, concatenação.
